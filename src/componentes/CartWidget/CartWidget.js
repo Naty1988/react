7a9dde9df722de1d626a644fb55eca6925d3d9b5
Natalia Rodriguez <natalia.rodriguez1@bue.edu.ar>
2022-03-19 19:41:09 -0300

    clase2.2Nav

--- src/componentes/CartWidget/CartWidget.js
+++ src/componentes/CartWidget/CartWidget.js
@@ -2,7 +2,7 @@ const CartWidget = () => {
     return (
         <div>
             <img src="https://cdn-icons-png.flaticon.com/512/107/107831.png" />
-            0
+            <div>0</div>
         </div>
     )
 }
